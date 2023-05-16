8a0078ed93e19412c8fe6776ef1689a2a0db858d
Imtiyaaz-D <imtdamon777@gmail.com>
2023-05-16 15:24:41 +0200

    fire

---
+++ script/code.js
@@ -0,0 +1,9 @@
+let arry= ['Python' , 'C++' , 'VB.Net' ,'Javascript' ,'Assembly']
+// let lastElement = arry.pop();
+// console.log(lastElement);
+
+// let middleElement = arry.pop()
+// console.log(middleElement);
+
+let middleEle = arry[Math.round((arry.length - 1) / 2)]
+console.log(middleEle);
