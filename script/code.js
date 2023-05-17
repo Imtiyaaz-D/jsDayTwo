831f23dc307378b5edf7bbf1e53a448e8087ab06
Imtiyaaz-D <imtdamon777@gmail.com>
2023-05-17 09:48:40 +0200

    day3

--- script/code.js
+++ script/code.js
@@ -1,20 +1,34 @@
-let arry= ['Python' , 'C++' , 'VB.Net' ,'Javascript' ,'Assembly','c#']
-// let lastElement = arry.pop();
-// console.log(lastElement);
-console.log(arry.at(-1))
+// let arry= ['Python' , 'C++' , 'VB.Net' ,'Javascript' ,'Assembly','c#']
+// // let lastElement = arry.pop();
+// // console.log(lastElement);
+// console.log(arry.at(-1))
 
-// let middleElement = arry.pop()
-// console.log(middleElement);
+// // let middleElement = arry.pop()
+// // console.log(middleElement);
 
-// let middleEle = arry[Math.round((arry.length - 1) / 2)]
-// console.log(middleEle);
-//5/2=2.5
-// let x = math.floor(arry.length/2)
-// console.log(arry[x])
+// // let middleEle = arry[Math.round((arry.length - 1) / 2)]
+// // console.log(middleEle);
+// //5/2=2.5
+// // let x = math.floor(arry.length/2)
+// // console.log(arry[x])
 
-let x = Math.floor(arry.length/2)
-if(x % 2 == 0){
-    console.log(arry[x])
-}else{
-    console.log(arry.slice(x-1,x=1))
-}
+// let x = Math.floor(arry.length/2)
+// if(x % 2 == 0){
+//     console.log(arry[x])
+// }else{
+//     console.log(arry.slice(x-1,x=1))
+// }
+
+//Day 3 exerices
+// let data = 'you';console.log(typeof(data))string
+let data = 67;console.log(typeof(data))//number(it changed)
+
+let x = 89;console.log(typeof(x));//number
+
+let firstName = 'Imti';console.log(typeof(firstName))//string
+
+let gender = true;console.log(typeof(gender))//boolean
+
+let surname;console.log(typeof(surname))//undefined
+
+let age = null;console.log(typeof(age))//object 
